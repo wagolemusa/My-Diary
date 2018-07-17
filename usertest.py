bb97b91ee5c7dd7676eb58ff2bf47b13156f32f5
kisumutalent <homiemusa@gmail.com>
2018-07-17 18:56:30 +0300

    [feature #159102162] view all users

--- usertest.py
+++ usertest.py
@@ -5,7 +5,7 @@ import json
 
 class UserTestCase(unittest.TestCase):
 
-
+	"""Get all users """
 	def test_get_all_users(self):
 		tester = app.test_client(self)
 		response = tester.get('/api/v1/get_all_users')
