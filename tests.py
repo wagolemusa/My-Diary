148a753bac1e27134df34c4009fdeb247d8a1f14
kisumutalent <homiemusa@gmail.com>
2018-07-17 11:46:05 +0300

    adding travis file

--- tests.py
+++ tests.py
@@ -19,6 +19,20 @@ class EntriesTestCase(unittest.TestCase):
 		response = tester.get('api/V1/view_entry/1')
 		self.assertEqual(response.status_code, 200)
 
+		""" Test post Entry"""
+	#def post_entery(self, title, date, entry):
+	#	return self.app.post(
+	#		'/api/v1/post_entry',
+	#		dics=entry(title=title, date=date, entry=entry)
+	#	)
+	#def test_post__entry(self):
+	#	response = self.post_entry('hiphop', '23/08/2018', 'I love HipHop')
+	#		self.assertEqual(response.status_code, 200)
+	#def test_post_entry(self):
+	#	tester = app.test_client(self)
+	#	response = tester.post('/api/v1/post_entry')
+	#	self.assertEqual(response.status_code, 200)
+
 
 
 if __name__ == '__main__':
