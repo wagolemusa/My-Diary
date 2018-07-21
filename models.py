0e3152b21009d1caf93a53598e9dac5c7f401f3e
kisumutalent <homiemusa@gmail.com>
2018-07-21 23:00:40 +0300

    [feature #159214781] database connection

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
 import psycopg2
 
-
+def dbcontaction():
+	dbcon = 'dbname=challenge3 user=postgres password=refuge host=localhost'
+	try:
+		return psycopg2.connect(dbcon)
+	except:
+		print("Database connection failed")
