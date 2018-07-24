c7565a93bd04e2cd1895e72e119d3f8bd5201e6a
kisumutalent <homiemusa@gmail.com>
2018-07-24 16:03:31 +0300

    create user login

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from models import *
 import psycopg2
-
+from passlib.hash import sha256_crypt
+import jwt
+import datetime
 app = Flask(__name__)
 
 
@@ -21,12 +23,11 @@ def home():
 """User Register"""
 @app.route('/api/v2/auth/signup', methods=['POST'])
 def register():
-#@dbcon.commit()
 	if request.method == 'POST':
 		full_name = request.get_json()['full_name']
 		username  = request.get_json()['username']
 		email     = request.get_json()['email']
-		password  = request.get_json()['password']
+		password  = sha256_crypt.encrypt(str(request.get_json()['password']))
 		dbcur.execute("INSERT INTO users(full_name, username, email, password) VALUES(%s, %s, %s, %s)",(full_name, username, email, password))
 		dbcon.commit()
 	return jsonify({"message": 'Succefuly Registerd'}), 200
@@ -35,20 +36,18 @@ def register():
 @app.route('/api/v2/auth/signin', methods=['GET', 'POST'])
 def signin():
 	if request.method == 'POST':
-
 		username = request.get_json()['username']
 		password = request.get_json()['password']
 		dbcur = dbcon.cursor()
-		result = dbcur.execute("SELECT * FROM users WHERE username = %s", [username])
-		#if result > 1:
-		data = dbcur.fetchone()
-		#password = data['password']
-		dbcon.close()
+		result = dbcur.execute("SELECT * FROM users WHERE username = %s;", [username])
+		data=result.fetchone()
+		if data is not None and password == data['password']:
+			token = jwt.encode({"username":username, "password":password, "exp":datetime.datetime.utcnow()+datetime.timedelta(minutes=20)},app.config['SECRET_KEY'])
+			return jsonify({"token":token.decode('utf-8')})
+		return jsonify("Invaild Creditaions")
+	return jsonify("Method not allowed")
 
-		#else:
-	return jsonify({"message": 'Username not found'})
-	return jsonify({"message": 'Welcome to my Diary'})
-
+"""Post Entries"""
 @app.route('/api/v2/entry', methods=['POST'])
 def post_entry():
 	if request.method == 'POST':
@@ -59,37 +58,32 @@ def post_entry():
 		dbcon.commit()
 	return jsonify({"message": 'Successfuly Posted Entries'})
 
-
+""" Get all Entries"""
 @app.route('/api/v2/get_entries', methods=['GET'])
 def get_entries():
 	dbcur.execute("SELECT * FROM entries")
 	entries  = dbcur.fetchall()
 	return jsonify(entries)
 
+"""View one entry"""
 @app.route('/api/v2/view_an_entry/<int:id>', methods=['GET'])
 def view_an_entry(id):
 	dbcur.execute("SELECT * FROM 	entries WHERE id = %s", [id])
 	entries = dbcur.fetchone()
 	return jsonify(entries)
 
-
+"""Update Entries"""
 @app.route('/api/v2/update_an_entry/<int:id>', methods=['PUT'])
 def update_entry(id):
-	#dbcur = dbcon.cursor()
 	dbcur.execute("SELECT * FROM 	entries WHERE id = %s", [id])
 	entries = dbcur.fetchone()
 	if request.method == 'PUT':
 		title = request.get_json()['title']
 		dates = request.get_json()['dates']
 		entries = request.get_json()['entries']
-
 		dbcur.execute("UPDATE entries SET title=%s, dates=%s, entries=%s WHERE id=%s",(title, dates, entries, id))
 		dbcon.commit()
 	return jsonify({"massege": "Entries Succesfuly Updated"})
-
-
-
-
 
 if __name__ == '__main__':
 	app.run()
