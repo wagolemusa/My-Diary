87bc0f31ebd38b0ef38c87df96eb7c9aaf00fde5
kisumutalent <homiemusa@gmail.com>
2018-07-24 10:50:30 +0300

    modify add entry, user signup and create test file

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify, request
 from models import *
+import psycopg2
 
 app = Flask(__name__)
 
+
 app.config['SECRET_KEY'] = 'refuge'
+
+dbcon = psycopg2.connect(dbname='mydiary', user='postgres', password='refuge', host='localhost')
+
+dbcur = dbcon.cursor()
 
 
 
@@ -21,7 +27,6 @@ def register():
 		username  = request.get_json()['username']
 		email     = request.get_json()['email']
 		password  = request.get_json()['password']
-		dbcur = dbcon.cursor()
 		dbcur.execute("INSERT INTO users(full_name, username, email, password) VALUES(%s, %s, %s, %s)",(full_name, username, email, password))
 		dbcon.commit()
 	return jsonify({"message": 'Succefuly Registerd'}), 200
@@ -46,18 +51,18 @@ def signin():
 
 @app.route('/api/v2/entry', methods=['POST'])
 def entry():
-	dbcon.commit()
 	if request.method == 'POST':
 		title = request.get_json()['title']
 		dates = request.get_json()['dates']
 		entries = request.get_json()['entries']
-		dbcur = dbcon.cursor()
 		dbcur.execute("INSERT INTO entries(title, dates, entries) VALUES(%s, %s, %s )",(title, dates, entries))
 		dbcon.commit()
 	return jsonify({"message": 'Successfuly Posted Entries'})
 
 
- 
+
+
+
 
 if __name__ == '__main__':
 	app.run()
