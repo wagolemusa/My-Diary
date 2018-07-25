dbed1d6a190d53611e162b7d83fd4abd9452b105
kisumutalent <homiemusa@gmail.com>
2018-07-25 08:55:04 +0300

    test for post entries

--- test_entry.py
+++ test_entry.py
@@ -13,6 +13,12 @@ class UserTestCase(unittest.TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertIn(b'Welcome To Home Page', response.data)
 
+  def test_post_entries(self):
+    tester = app.test_client(self)
+    response = tester.post('/api/v2/entry', content_type="application/json")
+    data=dict(title="python", dates="04/08/2018", entries="I will have to code")
+    self.assertIn(b'Successfuly Posted Entries', response.data)
+
 
 if __name__ == '__main__':
   unittest.main()
